0bab9089ee58d8b2bbcb1e0b9dfaf52258aa2a60
Alessandro David <alessandrodp2222@gmail.com>
2025-09-23 16:24:47 -0500

    Add MoviesGrid component to display movie count

---
+++ src/components/MoviesGrid.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import "../styles.css";
+
+export default function MoviesGrid() {
+
+    const [movies, setMovies] = useState([]);
+
+
+    useEffect(() => {
+
+        const m = ["a", "b", "c", "d"];
+        setMovies(m);
+
+    }, []);
+
+    return (    
+        <div>{movies.length}</div>
+    );
+}   
